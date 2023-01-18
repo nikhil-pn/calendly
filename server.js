93c9522992ab2178f1b06648e519cb37e3879e53
nikhilpn725 <nikhilpn725@gmail.com>
2023-01-18 21:23:46 +0530

    Routes added

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const app = (express = express());
+
+const { connectDB } = require("./config/db");
+
+const userRoutes = require("./routes/userRoutes")
+const scheduleRoutes = require("./routes/scheduleRoutes")
+const eventRoutes = require("./routes/eventRoutes")
+
+
+require("dotenv").config();
+connectDB();
+app.set(cors());
+
+
+app.use("/api/v1/user", userRoutes)
+app.use("/api/v1/schedule", scheduleRoutes)
+app.use("/api/v1/event", eventRoutes)
+
+
+app.listen(3001, () => {
+  console.log("Server is running at 3001");
+});
